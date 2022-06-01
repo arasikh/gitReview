668799ad6e37ed6742182a5cc7c5d3d4c53fc4a1
arasikh <rasikhmazda@gmail.com>
2022-05-31 21:35:39 -0400

    1st commit

---
+++ src/gitReview/gitReview01.java
@@ -0,0 +1,11 @@
+package gitReview;
+
+public class gitReview01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("today we learn github");
+		System.out.println("today is a very nice day");
+	}
+
+}
